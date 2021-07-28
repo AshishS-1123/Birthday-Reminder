c631cebf3246245ffdec21dcfc53bb5d30e80f51
Ashish Shevale <shevaleashish@gmail.com>
2021-07-28 14:33:47 +0530

    Populate List component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,23 @@
 import React from "react"
 import "./List.css"
 
-function List() {
+function List({items}) {
+    console.log(items)
     return (
-        <div>
-            List
+        <div className="List__card">
+            {items.map(item => {
+                const {id, name, age, image} = item
+                const initials = name.split(" ")[0][0] + name.split(" ")[1][0]
+                return (
+                    <div className="List__item_card" key={id}>
+                        <img src={image}className="List__image" alt={initials} />
+                        <div className="List__data">
+                            <p className="List__name">{name}</p>
+                            <p className="List__age">{age}</p>
+                        </div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
